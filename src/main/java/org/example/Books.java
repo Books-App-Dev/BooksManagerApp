08f42c2c8363991e28520c483bb76ab7b6e5bfc4
FranGarciaga <frangg_10@hotmail.com>
2024-10-29 13:23:50 +0100

    Fix Input

--- src/main/java/org/example/Books.java
+++ src/main/java/org/example/Books.java
@@ -4,16 +4,14 @@ import java.util.Scanner;
 
 public class Books {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         String number1 = "1";
         String number2 = "2";
         String number3 = "3";
         String number4 = "4";
         String number5 = "5";
 
-        Scanner number = new Scanner(System.in);
 
-        String numberInput = number.nextLine();
 
         System.out.println("Gestor de Libros Técnicos de Programación");
         System.out.println("1. Añadir libro");
@@ -21,7 +19,11 @@ public class Books {
         System.out.println("3. Eliminar libro");
         System.out.println("4. Cambiar repositorio");
         System.out.println("5. Salir");
-        System.out.println("Seleccione una opción: " + numberInput);
+        System.out.println("Seleccione una opción:");
+
+        Scanner number = new Scanner(System.in);
+        String numberInput = number.nextLine();
+
 
     }
 }
